604b6eddc41e6f4e2f14a0027a2a979ba6e7d8d1
Andreas Huber <andreashuber69@users.noreply.github.com>
2024-04-21 14:37:02 +0200

    style: limit max line length

--- src/FibonacciWorker.ts
+++ src/FibonacciWorker.ts
@@ -2,7 +2,7 @@ import { implementWorker } from "kiss-worker";
 
 // The function we want to execute on a worker thread (worker function)
 const getFibonacci = (n: number): number =>
-    ((n < 2) ? Math.floor(n) : getFibonacci(n - 1) + getFibonacci(n - 2));
+    (n < 2) ? Math.floor(n) : getFibonacci(n - 1) + getFibonacci(n - 2);
 
 export const FibonacciWorker = implementWorker(
     // A function that creates a web worker running this script
