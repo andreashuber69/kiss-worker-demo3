fab4603f3cf14aac23b1ca2e1f186866ad49e925
Andreas Huber <andreashuber69@users.noreply.github.com>
2024-05-04 11:57:43 +0200

    style: fix eslint issues

--- src/createFibonacciWorker.ts
+++ src/createFibonacciWorker.ts
@@ -2,13 +2,13 @@ import { implementFunctionWorker } from "kiss-worker";
 
 // The function we want to execute on a worker thread
 const fibonacci = (n: number): number =>
-    (n < 2) ? Math.floor(n) : fibonacci(n - 1) + fibonacci(n - 2);
+    ((n < 2) ? Math.floor(n) : fibonacci(n - 1) + fibonacci(n - 2));
 
 export const createFibonacciWorker = implementFunctionWorker(
     // A function that creates a web worker running this script
     () => new Worker(
         new URL("createFibonacciWorker.js", import.meta.url),
-        { type: "module" }
+        { type: "module" },
     ),
     fibonacci,
 );
