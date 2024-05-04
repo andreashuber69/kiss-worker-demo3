2c34eddb9f5d0b33ee94c2cc93a06eb247e54ab2
Andreas Huber <andreashuber69@users.noreply.github.com>
2024-05-04 09:53:36 +0200

    style: reformat

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,13 @@ import { createFibonacciWorker } from "./createFibonacciWorker.ts";
 
 // Start a new worker thread waiting for work.
 const worker = createFibonacciWorker();
-// Send the argument (40) to the worker thread, where it will be
-// passed to our function. In the mean time we're awaiting the
-// returned promise, which will eventually fulfill with the result
-// calculated on the worker thread.
+
+// Send the argument (40) to the worker thread, where it will be passed
+// to our function. In the mean time we're awaiting the returned promise,
+// which will eventually fulfill with the result calculated on the worker
+// thread.
 const result = await worker.execute(40);
+
 const element = document.querySelector("h1");
 
 if (element) {
