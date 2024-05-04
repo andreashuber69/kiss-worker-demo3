cc4883d34eee3e967a249fe6d93e8ac25ed1253a
Andreas Huber <andreashuber69@users.noreply.github.com>
2024-05-04 09:47:30 +0200

    refactor: move script to main.ts

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import { createFibonacciWorker } from "./createFibonacciWorker.ts";
+
+// Start a new worker thread waiting for work.
+const worker = createFibonacciWorker();
+// Send the argument (40) to the worker thread, where it will be
+// passed to our function. In the mean time we're awaiting the
+// returned promise, which will eventually fulfill with the result
+// calculated on the worker thread.
+const result = await worker.execute(40);
+const element = document.querySelector("h1");
+
+if (element) {
+    element.textContent = `${result}`;
+}
